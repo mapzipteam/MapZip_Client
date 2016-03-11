bee61da6b2072cbfe98f46bb95b84d7063a75a31
yucaroll <ghldjfldpsej@naver.com>
2016-03-11 18:11:58 +0900

    JOIN 포맷 수정

--- app/src/main/java/com/mapzip/ppang/mapzipproject/network/ResponseUtil.java
+++ app/src/main/java/com/mapzip/ppang/mapzipproject/network/ResponseUtil.java
@@ -7,4 +7,5 @@ public class ResponseUtil {
 
     public final static int PROCESS_APPLICATION_SETUP = 1;
     public final static int PROCESS_JOIN = 2;
+    public final static int PROCESS_LOGIN = 3;
 }
